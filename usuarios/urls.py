5d6c5a9943905f6d7d8f44f91875ae14b2a5bda7
Antonella <antomuttib@gmail.com>
2024-02-26 20:59:40 -0300

    34fd

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import SingUpView
 from .views import LoginView
+from .views import DeleteUser
 
 
 app_name = 'usuarios'
@@ -8,5 +9,6 @@ app_name = 'usuarios'
 urlpatterns = [
     path('', SingUpView.as_view(), name='registro'),
     path('login/', LoginView.as_view(), name='login'),
+    path('borrar_cuenta/<int:pk>', DeleteUser.as_view(), name='delete')
 ]
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import get_user_model
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from .forms import UserCreationFormWithEmail
-from django.contrib.auth import views as auth_views
+from django.views.generic import DeleteView
 from .forms import LoginForm
-from django.contrib.auth import get_user_model
+from .forms import UserCreationFormWithEmail
 
 
 class SingUpView(CreateView):
@@ -24,3 +26,10 @@ class LoginView(auth_views.LoginView):
         if 'next' in self.request.GET:
             return HttpResponseRedirect(self.request.GET['next'])
         return response
+    
+class DeleteUser(SuccessMessageMixin, DeleteView):
+    model = get_user_model()
+    template_name = 'usuarios/eliminar_cuenta_confirmacion.html'
+    success_url = reverse_lazy('usuarios:login')
+    success_message = 'Tu cuenta fue eliminada de Otra App de Notas'
+
